60716140e9d461072ce1ef6609bbaac5241885a7
Pankaj Saratkar <108680739+PANKAJ11111111@users.noreply.github.com>
2023-03-25 06:23:32 +0530

    Add files via upload
    complex classs using c++

---
+++ complex_class_oops.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class ComplexNumbers {
+    // Complete this class
+private :
+int real;
+int imaginary;
+
+public :
+
+ComplexNumbers(int r , int i){
+    real =r;
+    imaginary =i;
+}
+
+void print(){
+    cout<<real<<" + i"<<imaginary<<endl;
+}
+
+void plus(ComplexNumbers c2){
+    real = real + c2.real;
+    imaginary = imaginary + c2.imaginary;
+}
+
+void multiply(ComplexNumbers c2){
+    int a = real * c2.real;
+    int b = real * c2.imaginary;
+    int c = imaginary * c2.real;
+    int d = (imaginary * c2.imaginary) * -1;
+
+    real = a +d;
+    imaginary = b+c;
+
+
+}
+  
+};
+
+int main() {
+    int real1, imaginary1, real2, imaginary2;
+    
+    cin >> real1 >> imaginary1;
+    cin >> real2 >> imaginary2;
+    
+    ComplexNumbers c1(real1, imaginary1);
+    ComplexNumbers c2(real2, imaginary2);
+    
+    int choice;
+    cin >> choice;
+    
+    if(choice == 1) {
+        c1.plus(c2);
+        c1.print();
+    }
+    else if(choice == 2) {
+        c1.multiply(c2);
+        c1.print();
+    }
+    else {
+        return 0;
+    }
+    
+}
+ 
+
+
